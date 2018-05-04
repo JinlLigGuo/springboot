391146ce822c2d58661cd5cd7a82fa3eebef29e6
JinlLigGuo <kin__1993@163.com>
2018-05-04 22:32:39 +0800

    jin
    静态文件配置

--- src/main/java/com/springboot/config/StaticWebMvcConfigurerAdapter.java
+++ src/main/java/com/springboot/config/StaticWebMvcConfigurerAdapter.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class GoWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
+public class StaticWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
